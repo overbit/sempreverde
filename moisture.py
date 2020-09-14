2900a67edc5239899f110f2c9b7e4042811899e0
overbit <2861984+overbit@users.noreply.github.com>
2020-09-14 19:36:53 +0100

    Fix renaming

--- moisture.py
+++ moisture.py
@@ -3,8 +3,7 @@
 # Start by importing the libraries we want to use
 
 import RPi.GPIO as GPIO # This is the GPIO library we need to use the GPIO pins on the Raspberry Pi
-import time # This is the time library, we need this so we can use the sleep function
-
+import time
 # This is our callback function, this function will be called every time there is a change on the specified GPIO channel, in this example we are using 17
 
 def callback_action(channel):  
@@ -19,16 +18,19 @@ def callback_action(channel):
 GPIO.setmode(GPIO.BCM)
 
 # Define the GPIO pin that we have our digital output from our sensor connected to
-channel = 17
+sensor_channel = 17
 # Set the GPIO pin to an input
-GPIO.setup(channel, GPIO.IN)
+GPIO.setup(sensor_channel, GPIO.IN)
 
 # This line tells our script to keep an eye on our gpio pin and let us know when the pin goes HIGH or LOW
-GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300)
-# This line assigns a function to the GPIO pin so that when the above line tells us there is a change on the pin, run this function
-GPIO.add_event_callback(channel, callback_action)
+GPIO.add_event_detect(sensor_channel, GPIO.BOTH, bouncetime=300)
+# This line asigns a function to the GPIO pin so that when the above line tells us there is a change on the pin, run this function
+GPIO.add_event_callback(sensor_channel, callback_action)
 
 # This is an infinte loop to keep our script running
 while True:
 	# This line simply tells our script to wait 0.1 of a second, this is so the script does not hog all of the CPU
-	time.sleep(0.1)
+	time.sleep(0.5)
+
+
+GPIO.cleanup()
