0981e58a3963b0299097d97cafe24f7bdd28dc7b
overbit <2861984+overbit@users.noreply.github.com>
2020-10-20 23:02:12 +0100

    Update moisture

--- moisture-mcp3008.py
+++ moisture-mcp3008.py
@@ -3,15 +3,9 @@ import digitalio
 import board
 import adafruit_mcp3xxx.mcp3008 as MCP
 from adafruit_mcp3xxx.analog_in import AnalogIn
-from db import *
-
-initDb()
 
 if __name__ == '__main__':
     print("Starting sensor...")
-
-def moisture_perc(val):
-    return (1- val/3.3) * 100
 
 # create the spi bus
 spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
@@ -33,16 +27,29 @@ channels[5] = AnalogIn(mcp, MCP.P5)
 channels[6] = AnalogIn(mcp, MCP.P6)
 channels[7] = AnalogIn(mcp, MCP.P7)
 
+def moisture_perc(voltage):
+    return (1 - voltage/3.3) * 100
+
 # print('| Raw ADC Value  | ADC Voltage ')
 print('| {0:10.0f} | {1:10.0f} | {2:10.0f} | {3:10.0f} | {4:10.0f} | {5:10.0f} | {6:10.0f} | {7:10.0f} |'.format(*range(8)))
 
 import time
 while True:
+    # print ('\t\t\t\t\t\t {}'.format(perc))
+    # print('| {0.value:>4} | {1.value:>4} | {2.value:>4} | {3.value:>4} | {4.value:>4} | {5.value:>4} | {6.value:>4} | {7.value:>4} ||| {3.voltage:>4}'.format(*channels))
+
+    median=10
+    for x in range(median):
+        readingSum = [0]*8
+        for x in range(8):
+            readingSum[x] = readingSum[x] + channels[x].voltage
+        time.sleep(0.5)
+    
+    for x in range(8):
+        readingSum[x] = readingSum[x] /median
+
     readingPerc = [0]*8
     for x in range(8):
         readingPerc[x] = "{:10.4f}".format(round(moisture_perc(channels[x].voltage), 3))
     print('| {0:>4} | {1:>4} | {2:>4} | {3:>4} | {4:>4} | {5:>4} | {6:>4} | {7:>4} |'.format(*readingPerc))
-    # print ('\t\t\t\t\t\t {}'.format(perc))
-    # print('| {0.value:>4} | {1.value:>4} | {2.value:>4} | {3.value:>4} | {4.value:>4} | {5.value:>4} | {6.value:>4} | {7.value:>4} ||| {3.voltage:>4}'.format(*channels))
-
-    time.sleep(1)
+    
--- moisture.py
+++ moisture.py
@@ -2,44 +2,49 @@ import busio
 import digitalio
 import board
 import adafruit_mcp3xxx.mcp3008 as MCP
+import time
 from adafruit_mcp3xxx.analog_in import AnalogIn
-from db import *
+from data import *
 
-initDb()
+class Reading:
+    def __init__(self, value, voltage):
+        self.value = value
+        self.voltage = voltage
+        self.percentage = round((1- voltage/3.3) * 100, 2)
+
+class Sensor:
+    @staticmethod
+    def Start():
+        print("Starting sensor...")
+
+        # create the spi bus
+        spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
+
+        # create the cs (chip select)
+        cs = digitalio.DigitalInOut(board.D5)
+
+        # create the mcp object
+        mcp = MCP.MCP3008(spi, cs)
+
+        # create an analog input channel on pin 0
+        channel = AnalogIn(mcp, MCP.P3)
+
+        reading =  Reading(value=channel.value, voltage=channel.voltage)
+        SensorRepository.SaveReading(percentage=reading.percentage, value=reading.value, voltage=reading.voltage)
+
+        while True:
+            avg =  [0]*2
+            for x in range(50):
+                avg[0]= avg[0] + channel.value
+                avg[1]= avg[1] + channel.voltage
+                time.sleep(0.5)
+                # print('{} | {}'.format(x, str(channel.value)))
+
+            avgValue = avg[0] / 50
+            avgVoltage = avg[1] / 50
+
+            reading =  Reading(value=avgValue, voltage=avgVoltage)
+            SensorRepository.SaveReading(percentage=reading.percentage, value=reading.value, voltage=reading.voltage)
 
 if __name__ == '__main__':
-    print("Starting sensor...")
-
-
-# create the spi bus
-spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
- 
-# create the cs (chip select)
-cs = digitalio.DigitalInOut(board.D5)
- 
-# create the mcp object
-mcp = MCP.MCP3008(spi, cs)
- 
-# create an analog input channel on pin 0
-channel = AnalogIn(mcp, MCP.P3)
-
-print('| Raw ADC Value  | ADC Voltage ')
-
-import time
-while True:
-    sensorValue = 0 
-    for x in range(50):
-        sensorValue = sensorValue + channel.value; 
-        time.sleep(0.5)
-        print('{} | {}'.format(x, str(channel.value)))
-        # print('| {}\t\t | {}V\t\t || {}'.format(str(channel.value), str(round(channel.voltage, 5)), x))
-
-
-    sensorValue = sensorValue/50.0; 
-    print('>>>>>>>>>>>>>> | {}\t\t | <<<<<<<<<<<<<<'.format(str(channel.value)))
-    
-    reading = SensorReading(sensorName='moisture', value=channel.value, voltage=channel.voltage)
-    reading.save()
-    # insert_reading("MoistureSensor", channel.value, channel.voltage)
-    reading.select()
-    time.sleep(5)
+    Sensor.Start()
